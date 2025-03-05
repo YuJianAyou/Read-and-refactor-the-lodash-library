60f066515f1f99eccdf5b9d6139f0bd5e4f8553e
wuwei <2306400714@qq.com>
2025-03-05 13:09:06 +0800

    2025/3/5

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,19 +1,15 @@
 import { describe, it, expect } from 'vitest';
-
-
-
 import _ from "../src/index"
 
 
-
-
-
-
 describe('判断函数测试', () => {
+
     it('isArray', () => {
-        expect(_.isArray([1])).toBeTruthy(); //true
+        expect(_.isArray([])).toBeTruthy(); //true
         expect(_.isArray({})).toBeFalsy(); //false
         expect(_.isArray(1)).toBeFalsy(); //false
         expect(_.isArray("")).toBeFalsy(); //false
     });
+
+
 });
